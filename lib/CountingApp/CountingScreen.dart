02fb07b7814bf4e228c6af471f3c1a355e153da7
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-04-21 11:19:45 +0600

    convert the height and width responsivly

--- lib/CountingApp/CountingScreen.dart
+++ lib/CountingApp/CountingScreen.dart
@@ -3,41 +3,39 @@ import 'package:flutter/material.dart';
 import 'customWidget.dart';
 import 'package:get/get.dart';
 
-class CountingScreen extends StatefulWidget {
-  const CountingScreen({super.key});
+class CountingScreen extends StatelessWidget {
+  CountingScreen({super.key});
 
-  @override
-  State<CountingScreen> createState() => _CountingState();
-}
-
-class _CountingState extends State<CountingScreen> {
   CounterController controller = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    final screenWidth = MediaQuery.sizeOf(context).width;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.green[100],
         appBar: AppBar(
           backgroundColor: Colors.blue,
           centerTitle: true,
-          toolbarHeight: 80,
+          toolbarHeight: screenHeight * 0.09,
           title: Text(
             "Counting App",
             style: TextStyle(
-                fontSize: 40,
+                fontSize: screenHeight * 0.06,
                 fontStyle: FontStyle.italic,
                 fontWeight: FontWeight.w600,
                 color: Colors.white),
           ),
           leading: IconButton(
               hoverColor: Colors.amber,
-              iconSize: 40,
+              iconSize: screenHeight * 0.06,
               onPressed: (){},
               icon: Icon(Icons.menu,color: Colors.white)
           ),
           actions: [
             IconButton(
-                iconSize: 40,
+                iconSize: screenHeight * 0.06,
                 hoverColor: Colors.amber,
                 onPressed: (){},
                 icon: Icon(Icons.search_sharp,color: Colors.white))
@@ -45,70 +43,73 @@ class _CountingState extends State<CountingScreen> {
         ),
 
         body: Center(
-          child: Container(
-            height: 500,
-            width: 400,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(width: 2),
-              color: Colors.lime[100]
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Press the button to showing Counting",
-                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 16)),
-                SizedBox(
-                  height: 10,
-                ),
-                Obx((){
-                  return Text("Show the Counting Result: ${controller.count.toString()}",
-                      style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20,color: Colors.red[300]));
-                }),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      controller.Increment();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        "Increment",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                      ),
-                    )),
-                SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      controller.Decrement();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        "Decrement",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                      ),
-                    )),
-                SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      controller.Clear();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        "Clear",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                      ),
-                    )),
-              ],
+          child: Padding(
+            padding: EdgeInsets.all(screenWidth * 0.04),
+            child: Container(
+              height: screenHeight * 0.8,
+              width: screenWidth * 0.7,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(width: screenWidth * 0.001),
+                color: Colors.lime[100]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Press the button to showing Counting",
+                      style: TextStyle(fontWeight: FontWeight.w800, fontSize: screenHeight * 0.025)),
+                  SizedBox(
+                    height: screenHeight * 0.01,
+                  ),
+                  Obx((){
+                    return Text("Show the Counting Result: ${controller.count.toString()}",
+                        style: TextStyle(fontWeight: FontWeight.w800, fontSize: screenHeight * 0.03,color: Colors.red[300]));
+                  }),
+                  SizedBox(
+                    height: screenHeight * 0.01,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        controller.Increment();
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(screenWidth * 0.02),
+                        child: Text(
+                          "Increment",
+                          style: TextStyle(fontSize: screenHeight * 0.03, fontWeight: FontWeight.w800),
+                        ),
+                      )),
+                  SizedBox(
+                    height: screenHeight * 0.03,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        controller.Decrement();
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(screenWidth * 0.02),
+                        child: Text(
+                          "Decrement",
+                          style: TextStyle(fontSize: screenHeight * 0.03, fontWeight: FontWeight.w800),
+                        ),
+                      )),
+                  SizedBox(
+                    height: screenHeight * 0.03,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        controller.Clear();
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(screenWidth * 0.02),
+                        child: Text(
+                          "Clear",
+                          style: TextStyle(fontSize: screenHeight * 0.03, fontWeight: FontWeight.w800),
+                        ),
+                      )),
+                ],
+              ),
             ),
           ),
         ),
