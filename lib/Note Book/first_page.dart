31bc3fc2cdd74bd23b6698ea559634646fead4a8
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-04-27 10:33:38 +0600

    Initialized Firebase

--- lib/Note Book/first_page.dart
+++ lib/Note Book/first_page.dart
@@ -1,11 +1,17 @@
 import 'package:counting/Note%20Book/providers/note_provider.dart';
+import 'package:counting/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
 import 'pages/land page/land_page.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(  //initializing firebase for the project
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
     create: (_)=> NoteProvider(),
     child: MyApp(),));
